037bdeece96507db187925e1c892e49d1cfa67a2
Andrzej Baldys <andrzejbaldys1990@gmail.com>
2019-01-31 20:47:33 +0100

    listed software will be saved in csv file

--- list-installed-software.ps1
+++ list-installed-software.ps1
@@ -1,2 +1,5 @@
-﻿Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Format-Table –AutoSize
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Format-Table –AutoSize
+﻿Set-Variable -Name output -Value "C:\Users\$env:USERNAME\Documents\$env:USERNAME-tools.csv"
+If (Test-Path -Path $output) { Remove-Item $output }
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object -Property DisplayName, DisplayVersion | Export-Csv -Path $output -Append -NoTypeInformation
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object -Property DisplayName, DisplayVersion | Export-Csv -Path $output -Append -NoTypeInformation
+Write-Host "Output file location: " + $output
