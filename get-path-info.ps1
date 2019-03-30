8ca53cb4aecc5d2ab44757d271e330e976550d9b
Andrzej Baldys <andrzejbaldys1990@gmail.com>
2019-03-30 12:08:50 +0100

    path-info, system-info

---
+++ get-path-info.ps1
@@ -0,0 +1,9 @@
+﻿Set-Variable -Name output -Value "C:\Users\$env:USERNAME\Documents\$env:USERNAME-path.csv"
+If (Test-Path -Path $output) { Remove-Item $output }
+Set-Variable -Name paths -Value $env:Path.Split(';')
+foreach ($path in $paths) {
+   $path = '"' + $path + '",'
+   $path | Out-File -FilePath $output -Append
+ }
+Write-Host "Out put file location: " $output
+
---
+++ get-system-info.ps1
@@ -0,0 +1,7 @@
+﻿Set-Variable -Name output -Value "C:\Users\$env:USERNAME\Documents\$env:USERNAME-system.csv"
+If (Test-Path -Path $output) { Remove-Item $output }
+foreach ($key in $PSVersionTable.Keys) {
+   $info = '"' + $key + '","' + $PSVersionTable[$key] + '"'
+   $info | Out-File -FilePath $output -Append
+ }
+Write-Host "Out put file location: " $output
